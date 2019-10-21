fea6c8b0dd39ac3615f9af40a8c5cf68dcad3782
José Pereira <jmfp-b@live.com.pt>
2019-10-21 02:37:43 +0100

    Fixes

---
+++ IUE-Worksheet1/IUE-Worksheet1Tests/IUE_Worksheet1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  IUE_Worksheet1Tests.swift
+//  IUE-Worksheet1Tests
+//
+//  Created by José Pereira on 9/20/19.
+//  Copyright © 2019 José Pereira. All rights reserved.
+//
+
+import XCTest
+@testable import IUE_Worksheet1
+
+class IUE_Worksheet1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
